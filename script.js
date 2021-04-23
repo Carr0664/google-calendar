d2072fa6c46c9608f46efef00f3fe719161ebf0c
Damien Carr <carr0664@algonquinlive.com>
2021-04-23 15:57:21 -0400

    upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let intViewportHeight = window.innerHeight;
+let intViewportWidth = window.innerWidth;
+
+
+// setInterval(function () {
+//     let iframe = document.getElementById('iframe')
+//     let h = parseInt(intViewportHeight)
+//     let w = parseInt(intViewportWidth)
+//     iframe.style.height = h;
+//     iframe.style.width = w;
+// }, 1000)
+
+setInterval(function () {
+    window.location.reload();
+}, 300000);
+
+
+console.log(intViewportHeight)
+console.log(intViewportWidth)
